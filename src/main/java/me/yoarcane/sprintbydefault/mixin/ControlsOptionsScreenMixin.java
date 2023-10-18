87daa11e36d23d0774ec4d08e9cc926808cbeccb
Ayydan <arcanewastakenofficial@gmail.com>
2023-10-18 17:04:13 -0400

    Update to Minecraft 1.20.1 + Updated Gradle, Fabric , Yarn and Loom
    - Updated to Minecraft 1.20.1.
    - Updated Gradle, Fabric, Yarn and Loom to the latest versions.

--- src/main/java/me/yoarcane/sprintbydefault/mixin/ControlsOptionsScreenMixin.java
+++ src/main/java/me/yoarcane/sprintbydefault/mixin/ControlsOptionsScreenMixin.java
@@ -1,11 +1,10 @@
 package me.yoarcane.sprintbydefault.mixin;
 
 import me.yoarcane.sprintbydefault.config.SprintByDefaultConfig;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
-import net.minecraft.client.gui.screen.option.KeybindsScreen;
-import net.minecraft.client.gui.screen.option.MouseOptionsScreen;
 import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.ClickableWidget;
@@ -13,13 +12,11 @@ import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
-import org.apache.commons.lang3.Validate;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(ControlsOptionsScreen.class)
 public abstract class ControlsOptionsScreenMixin extends GameOptionsScreen
@@ -42,8 +39,18 @@ public abstract class ControlsOptionsScreenMixin extends GameOptionsScreen
 
             SprintByDefaultConfig.getInstance().writeConfig();
         });
+
         sprintByDefaultOptionWidget.setTooltip(Tooltip.of(Text.translatable("options.sprintByDefault.tooltip")));
 
         this.addDrawableChild(sprintByDefaultOptionWidget);
+    }
+
+    @ModifyArg(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/option/ControlsOptionsScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;", ordinal = 6))
+    public Element resizeAndRepositionDoneButton(Element par1)
+    {
+        return ButtonWidget.builder(ScreenTexts.DONE, (button) ->
+        {
+            this.client.setScreen(this.parent);
+        }).dimensions(this.width / 2 - 155, this.height / 3 - 23, 150, 20).build();
     }
 }
